7b5647130230e4d635e8a4b66a69f16fdfc0642a
ivanailijin <ilijin.ra104.2020@uns.ac.rs>
2024-04-24 22:52:54 +0200

    Api host update

--- Explorer/src/env/environment.ts
+++ Explorer/src/env/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiHost: 'https://localhost:44333/api/',
+    apiHost: 'http://localhost:8083/api/',
     mapBoxApiKey: 'pk.eyJ1Ijoiam92YW5kb3ppYyIsImEiOiJjbG8xeHc5cTUwNGNiMmpteGxmcHRvNzduIn0.Viw2qDgjAzpB2pbzcnpSQA',
   };
   
